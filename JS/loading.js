5a078fd1593d12f18740f791acf7d2ebdd80cbca
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-13 16:01:35 +0700

    Add files via upload

--- JS/loading.js
+++ JS/loading.js
@@ -1,6 +1,22 @@
 window.onload = function() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
 	if(!window.location.hash) {
 		window.location = window.location + '#AboutMe';
 		window.location.reload();
 	}
+}
+
+
+
+var refresh = window.localStorage.getItem('refresh');
+console.log(refresh);
+if (refresh===null){
+	window.location.reload();
+	window.localStorage.setItem('refresh', "1");
+}
+
+window.onbeforeunload = function () {
+window.scrollTo(0, 0);
+window.requestAnimationFrame();
 }
