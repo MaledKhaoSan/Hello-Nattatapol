5e8ea5360b81ffa68515260981451393211b63e9
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-23 17:18:13 +0700

    Update Animations.js

--- JS/Animations.js
+++ JS/Animations.js
@@ -20,7 +20,7 @@ ScrollTrigger.matchMedia({
         AboutMeIntrol.to(".AboutMe-Text3-Box", 2.5,{scale: 35, top:"-45%", ease: Power3.easeIn},"<+0.5")
         AboutMeIntrol.to(".AboutMe_Background3", 2.8,{scale: 4, left: "100%", ease: Power3.easeIn},"<")
 
-        AboutMeIntrol.to('.AboutMe_Background3', 1,{filter:"blur(4px)"},"<+1");
+        // AboutMeIntrol.to('.AboutMe_Background3', 1,{filter:"blur(4px)"},"<+1");
         AboutMeIntrol.to('.AboutMe_Background3', 1,{opacity: 0},"<+0.5");
 
 
@@ -31,7 +31,7 @@ ScrollTrigger.matchMedia({
         // AboutMeIntrol.to(".AboutMe-container, .AboutMe-container-Inside", 2, {scale: 1.1,ease: Power3.easeOut},"<+0.2");
         AboutMeIntrol.to("#intro", 1.2,{backgroundColor:"#272727", ease:Power3.easeOut},"<");
         AboutMeIntrol.from("#AboutMe-MainClip, #AboutMe-MainClip-Inside", 1.5, {borderRadius: "160% 500%",ease: Power3.easeOut},"<");
-        AboutMeIntrol.from('.linear-Portfolio-Header, .AboutMeButton', 1.7,{filter:"blur(8px)", opacity: 0,ease: Power3.easeOut},"<");
+        // AboutMeIntrol.from('.linear-Portfolio-Header, .AboutMeButton', 1.7,{filter:"blur(8px)", opacity: 0,ease: Power3.easeOut},"<");
         AboutMeIntrol.set("body",{overflowY: "scroll"},"<");
     },
 
@@ -45,18 +45,18 @@ ScrollTrigger.matchMedia({
           AboutMeIntrol.to('.AboutMe_Background1, .AboutMe-Text1-Box', 1,{opacity: 0},"<+1");
   
   
-          AboutMeIntrol.from('.AboutMe_Background2, .AboutMe-Text2-Box', 0.5,{filter:"blur(8px)"},">-0.5");
+          // AboutMeIntrol.from('.AboutMe_Background2, .AboutMe-Text2-Box', 0.5,{filter:"blur(8px)"},">-0.5");
           AboutMeIntrol.to(".AboutMe-Text2-Box", 2.3,{scale: 40, left: "-360%", ease: Power3.easeIn},"<+0.5")
           AboutMeIntrol.to(".AboutMe_Background2", 2.5,{scale: 4, left: "30%", ease: Power3.easeIn},"<")
   
           // AboutMeIntrol.to('.AboutMe_Background2', 0.6,{filter:"blur(4px)",ease: Power3.easeIn},"<+1");
           AboutMeIntrol.to('.AboutMe_Background2, .AboutMe-Text2-Box', 0.9,{opacity: 0},"<+1");
   
-          AboutMeIntrol.from('.AboutMe_Background3, .AboutMe-Text3-Box', 0.3,{filter:"blur(8px)"},">-0.3");
+          // AboutMeIntrol.from('.AboutMe_Background3, .AboutMe-Text3-Box', 0.3,{filter:"blur(8px)"},">-0.3");
           AboutMeIntrol.to(".AboutMe-Text3-Box", 2.5,{scale: 50, top:"-65%", ease: Power3.easeIn},"<+0.5")
           AboutMeIntrol.to(".AboutMe_Background3", 2.8,{scale: 4, left: "100%", ease: Power3.easeIn},"<")
   
-          AboutMeIntrol.to('.AboutMe_Background3', 1,{filter:"blur(4px)"},"<+1");
+          // AboutMeIntrol.to('.AboutMe_Background3', 1,{filter:"blur(4px)"},"<+1");
           AboutMeIntrol.to('.AboutMe_Background3, .AboutMe-Text3-Box', 1,{opacity: 0},"<+0.5");
   
           //AboutMe Introl
@@ -67,7 +67,7 @@ ScrollTrigger.matchMedia({
           // AboutMeIntrol.to(".AboutMe-container, .AboutMe-container-Inside", 2, {scale: 1.1,ease: Power3.easeOut},"<+0.2");
           AboutMeIntrol.to("#intro", 1.2,{backgroundColor:"#272727", ease:Power3.easeOut},"<");
           AboutMeIntrol.from("#AboutMe-MainClip, #AboutMe-MainClip-Inside", 1.5, {borderRadius: "160% 500%",ease: Power3.easeOut},"<");
-          AboutMeIntrol.from('.linear-Portfolio-Header, .AboutMeButton', 1.7,{filter:"blur(8px)", opacity: 0,ease: Power3.easeOut},"<");
+          // AboutMeIntrol.from('.linear-Portfolio-Header, .AboutMeButton', 1.7,{filter:"blur(8px)", opacity: 0,ease: Power3.easeOut},"<");
           AboutMeIntrol.set("body",{overflowY: "scroll"},"<");
           
   
