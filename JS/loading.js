b3b7237abb11d00500cd124f6ba43e912baaa4fd
Nttp <81904457+MaledKhaoSan@users.noreply.github.com>
2021-12-24 16:44:10 +0700

    Update loading.js

--- JS/loading.js
+++ JS/loading.js
@@ -9,37 +9,66 @@
 
 
 
-var refresh = window.localStorage.getItem('refresh');
-console.log(refresh);
-if (refresh===null){
-	window.location.reload();
-	window.localStorage.setItem('refresh', "1");
-}
-
-window.onbeforeunload = function () {
-window.scrollTo(0, 0);
-window.requestAnimationFrame();
-}
 
 
+	if(window.location.hash) {
+		// Fragment exists
+		var refresh = window.localStorage.getItem('refresh');
+		console.log("with Hash");
+		if (refresh===null){
+			window.location.reload();
+			window.localStorage.setItem('refresh', "1");
+		}
+		window.onbeforeunload = function () {
+			window.scrollTo(0, 0);
+			window.requestAnimationFrame();
+		}
+		$(window).bind('load', function() {
+			$('.overlay').addClass('loaded');
+			setTimeout(function() {
+				$('.overlay').css({'display':'none'})
 
+			}, 2900)
+		});
+
+	  } else {
+		// Fragment doesn't exist
+	  }
+
+
+
+
+
+
+// $(document).ready(function() {
+// 	// Will remove overlay for users cannnot load properly.
+// 	setTimeout(function() {
+// 		$('.overlay').addClass('loaded');
+// 	}, 10000);
+
+// 	// Will wait for everything on the page to load.
+// 	$(window).bind('load', function() {
+// 		$('.overlay').addClass('loaded');
+// 		setTimeout(function() {
+// 			$('.overlay').css({'display':'none'})
+
+// 		}, 10000)
+// 		if(!window.location.hash) {
+// 			window.location = window.location + '#AboutMe';
+// 			window.location.reload();
+// 		}
+// 	})
+// })
 
 $(document).ready(function() {
-	// Will remove overlay for users cannnot load properly.
-	setTimeout(function() {
-		$('.overlay').addClass('loaded');
-	}, 10000);
+	if(!window.location.hash) {
+		window.location = window.location + '#AboutMe';
+		$(window).bind('load', function() {
+			$('.overlay').addClass('loaded');
+			setTimeout(function() {
+				$('.overlay').css({'display':'none'})
 
-	// Will wait for everything on the page to load.
-	$(window).bind('load', function() {
-		$('.overlay').addClass('loaded');
-		setTimeout(function() {
-			$('.overlay').css({'display':'none'})
-
-		}, 10000)
-		if(!window.location.hash) {
-			window.location = window.location + '#AboutMe';
-			window.location.reload();
-		}
-	})
+			}, 2900)
+		});
+	}
 })
